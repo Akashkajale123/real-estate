a7e0bf1bdf832180a78d129235824f2c0abd368c
Akash Kajale <akashkajale125@gmail.com>
2024-02-01 15:39:23 +0530

    home_page wip

--- frontend/src/components/property_pages/Home.jsx
+++ frontend/src/components/property_pages/Home.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 const Home = () => {
   return (
     <div className="main-box">
-    <div className="sidebar">
-        <h1 className='logo'>Logo</h1>
+      <div className="sidebar">
+        <h1 className="logo">Logo</h1>
         <ul>
-            <li > Property</li>
-            <li>Assistance</li>
-            <li>Received Interest</li>
-            <li>Sent Interest</li>
-            <li>Property Views</li>
-            <li>Tariff  Plan</li>
-           
+          <li>Property</li>
+          <li>Assistance</li>
+          <li>Received Interest</li>
+          <li>Sent Interest</li>
+          <li>Property Views</li>
+          <li>Tariff Plan</li>
         </ul>
+      </div>
+      <div className="right">
+        <div className="top-bar">
+          <h2 className="userid">USER ID : 06PPD125</h2>
+          <h2 className="user">User Name</h2>
+        </div>
+      </div>
     </div>
-    <div className="right">
-        <div className="top-bar"></div>
-        <div className="main-content"></div>
-    </div>
-    
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
