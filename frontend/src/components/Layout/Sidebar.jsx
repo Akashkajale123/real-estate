dc7d101eddc8096bae29832ff573c4c99bc9ca83
Akash Kajale <akashkajale125@gmail.com>
2024-02-04 00:32:06 +0530

    sidebar

--- frontend/src/components/Layout/Sidebar.jsx
+++ frontend/src/components/Layout/Sidebar.jsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   return (
 
       <aside className="sidebar">
-        <div style={{color: '#1A1B1C', fontSize: 35, fontFamily: 'Source Sans Pro', fontWeight: '700', wordWrap: 'break-word'}}>Logo</div>
+        <div style={{color: '#1A1B1C', fontSize: 35, fontFamily: 'Source Sans Pro', fontWeight: '700', wordWrap: 'break-word',textAlign:'center',marginTop:'45px'}}>Logo</div>
      {/* <div className="ul-container">
      <ul id='list'>
      <li id='property'>  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
